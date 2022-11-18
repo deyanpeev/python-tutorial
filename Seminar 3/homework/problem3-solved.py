ab247cbd171747dca82d2949479b884e17835f83
Deyan Peev <deqn.peev@gmail.com>
2022-11-18 23:18:48 +0200

    Problem 3 solved

---
+++ Seminar 3/homework/problem3-solved.py
@@ -0,0 +1,10 @@
+EMPTY_SPACE = ' '
+CHAR_TO_REPEAT = '*'
+
+lines_number = int(input("Enter a number: "))
+line = 0
+while line < lines_number - 1:
+    print(EMPTY_SPACE * (lines_number - line - 1) + CHAR_TO_REPEAT * (line * 2 + 1))
+    line+=1
+
+print(EMPTY_SPACE * (lines_number - 1) + CHAR_TO_REPEAT)
